3617d007bf48aa062e89fa240719c83ec8c05ca5
Alessandro Marzullo <amarzullo-sketch@users.noreply.github.com>
2026-04-21 16:22:35 -0400

    Add requirements and confusion matrix image

--- Code/train_model.py
+++ Code/train_model.py
@@ -2,7 +2,8 @@ from pathlib import Path
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
 
 base_path = Path(__file__).resolve().parent.parent
 input_path = base_path / "Cleaned Data" / "selected_flights.csv"
@@ -30,3 +31,14 @@ print("\nConfusion Matrix:")
 print(confusion_matrix(y_test, y_pred))
 print("\nClassification Report:")
 print(classification_report(y_test, y_pred))
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+image_path = BASE_DIR / "confusion_matrix.png"
+
+disp = ConfusionMatrixDisplay(confusion_matrix=confusion_matrix(y_test, y_pred))
+disp.plot()
+plt.title("Confusion Matrix")
+plt.savefig(image_path, bbox_inches="tight")
+plt.close()
+
+print(f"Saved confusion matrix image to: {image_path}")
